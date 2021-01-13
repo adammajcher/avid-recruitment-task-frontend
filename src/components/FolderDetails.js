57a41f27a3c3542590f670a8ef8f66a3383ae7a7
Adam <majcher.adam1@gmail.com>
2021-01-13 19:55:54 +0100

    handling limit, skip, query, folder finding by name

--- src/components/FolderDetails.js
+++ src/components/FolderDetails.js
@@ -1,29 +1,82 @@
 import axios from 'axios';
 import React from 'react';
 import ReactJson from 'react-json-view'
+import Alert from 'react-bootstrap/Alert'
 
 class FoldersDetails extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { Folder: {} };
+        this.state = {
+            Folder: {},
+            error: null,
+            skip: 0,
+            limit: 0,
+            query: ""
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
+        this.handleChange3= this.handleChange3.bind(this);
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.componentDidMount();
+    }
+    handleChange = (event) => {
+        this.setState({skip: event.target.value})
+    }
+    handleChange2 = (event) => {
+        this.setState({limit: event.target.value})
+    }
+    handleChange3 = (event) => {
+        this.setState({query: event.target.value})
     }
 
     componentDidMount() {
-        console.log(this.props.match);
         const { id } = this.props.match.params;
-        axios.get(`http://localhost:8080/${id}`)
+        axios.get(`http://localhost:8080/${id}`, {
+            params: {
+                skip: this.state.skip,
+                limit: this.state.limit,
+                query: this.state.query
+            }
+        })
             .then(res => {
                 const folder = res.data;
-                this.setState({ Folder: folder }, () => { console.log("po updejcie", this.state) });
+                this.setState({ Folder: folder });
             })
+            .catch((error) => this.setState({ error: error }));
     }
 
     render() {
+        if (this.state.error) {
+            return (
+                <div>
+                    <Alert variant="danger">
+                        <Alert.Heading>Folder not found (404 error).</Alert.Heading>
+                        Go back to <Alert.Link href="http://localhost:3000/">folders list</Alert.Link>.
+                    </Alert>
+                </div>
+            )
+        }
         return (
             <div>
-                <div>FoldersDetails</div>
-                <ReactJson src={ this.state.Folder } theme="monokai" />
+                <div>
+                    <h1>Folder details:</h1>
+                    <p>Go back to <a href="http://localhost:3000/">results list</a>.</p>
+                </div>
+                <form onSubmit = {this.handleSubmit}>
+                    Skip: < input type="number" defaultValue={this.state.skip} onChange={ this.handleChange }/> <br /> (0 default)
+                    Limit: < input type="number" defaultValue={this.state.limit} onChange={ this.handleChange2 }/> <br /> (0 brings full list)
+                    Query: <input type="text" defaultValue={this.state.query} onChange = {this.handleChange3}/> (will work only if take: "filemob", "masterclip", "subclip", "sequence", "group") <br />
+                    <input type="submit" value="Send request"/>
+                </form>
+
+                
+
+                <ReactJson src={this.state.Folder} theme="monokai" />
             </div>
         )
     }
--- src/components/FoldersList.js
+++ src/components/FoldersList.js
@@ -1,22 +1,66 @@
 import axios from 'axios';
 import React from 'react';
-import ReactJson from 'react-json-view'
 
 class FoldersList extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { folderInfos: [] };
+        this.state = {
+            folderInfos: [],
+            skip: 0,
+            limit: 0,
+            query: "",
+            folderToFind: "",
+        };
         this.onClickFolder = this.onClickFolder.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
+        this.handleChange3 = this.handleChange3.bind(this);
+        this.handleChange4 = this.handleChange4.bind(this);
+        this.submitFolder = this.submitFolder.bind(this);
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+        this.componentDidMount();
+    }
+    handleChange = (event) => {
+        if (event.target.value) {
+            this.setState({ skip: event.target.value })
+        }
+    }
+    handleChange2 = (event) => {
+        if (event.target.value) {
+            this.setState({ limit: event.target.value })
+        }
+    }
+    handleChange3 = (event) => {
+        this.setState({ query: event.target.value })
+    }
+    
+    handleChange4 = (event) =>{
+        this.setState({ folderToFind: event.target.value })
     }
 
     componentDidMount() {
-        console.log("siema", this.state)
-        axios.get('http://localhost:8080/')
+
+        axios.get(`http://localhost:8080/`, {
+            params: {
+                skip: this.state.skip,
+                limit: this.state.limit,
+                query: this.state.query
+            }
+        })
             .then(res => {
                 const folderInfos = res.data;
-                this.setState({ folderInfos: folderInfos.results }, ()=>{console.log("po updejcie", this.state)} );
-            })
+                this.setState({ folderInfos: folderInfos.results })
+            });
+    }
+
+    submitFolder(event) {
+        this.props.history.push(`/${this.state.folderToFind}`);
     }
 
     onClickFolder(folderId) {
@@ -26,12 +70,25 @@ class FoldersList extends React.Component {
     render() {
         return (
             <div>
-                <div>FoldersList</div>
+                <form onSubmit={this.handleSubmit}>
+                    Skip: < input type="number" defaultValue={this.state.skip} onChange={this.handleChange} /> (0 default)<br />
+                    Limit: < input type="number" defaultValue={this.state.limit} onChange={this.handleChange2} /> (0 brings full list) <br />
+                    Query: <input type="text" defaultValue={this.state.query} onChange={this.handleChange3} /> (eg. Demo)<br />
+                    <input type="submit" value="Send request" />
+                </form>
+
+                <h1><div>Results:</div></h1>
                 {
-                    this.state.folderInfos.map(folder => <div key={ folder.id } onClick={ ()=>{ this.onClickFolder(folder.id) } }>{JSON.stringify(folder)}</div>)
+                    this.state.folderInfos.map(folder => <div key={folder.id} onClick={() => { this.onClickFolder(folder.id) }}>{JSON.stringify(folder)}</div>)
                 }
+                <br></br>
+                <form onSubmit={this.submitFolder}>
+                    Folder name: <input type="text" defaultValue="" onChange={this.handleChange4} />  (eg. trolololo, 11, %2F%2Ftest-path%2FCloudUX1%2Fproj1%2Fproj1%20Bin.avb) <br />
+                    <input type="submit" value="Find folder" />
+                </form>
             </div>
-        )}
+        )
+    }
 }
 
 export default FoldersList
